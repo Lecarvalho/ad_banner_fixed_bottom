615d4891e0e2e89ccdd924e5e7ce1ffc90e44451
Leandro Carvalho <cadastro@lecarvalho.net>
2019-10-12 22:15:29 -0400

    ad banner

--- lib/adbanner_widget.dart
+++ lib/adbanner_widget.dart
@@ -1,13 +1,26 @@
+import 'dart:io';
+
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 
 class AdBannerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
       width: double.infinity,
-      color: Colors.amber,
-      child: Center(child: Text("AdBanner")),
+      child: AdmobBanner(
+        adSize: AdmobBannerSize.BANNER,
+        adUnitId: getBannerAdUnitId(),
+      ),
     );
+  }
+
+  String getBannerAdUnitId() {
+    if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/2934735716';
+    } else if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    }
+    return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
-import 'package:ad_banner_fixed_bottom/adbanner_widget.dart';
+import 'dart:io';
+
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'page1.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Admob.initialize(getAppId());
+  runApp(MyApp());
+}
+
+String getAppId(){
+  if (Platform.isIOS) {
+    return 'ca-app-pub-3940256099942544~1458002511';
+  } else if (Platform.isAndroid) {
+    return 'ca-app-pub-3940256099942544~3347511713';
+  }
+  return null;
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
